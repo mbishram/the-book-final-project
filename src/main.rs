e1def115c15abb95f1139e21589dd8e244538b30
Muh Bishram Y A A <m.bishram.yashir@gmail.com>
2025-02-14 08:55:35 +0800

    Chapter 21.1 done

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
+use std::fs;
+use std::io::{prelude::*, BufReader};
+use std::net::{TcpListener, TcpStream};
+
 fn main() {
-    println!("Hello, world!");
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+
+        handle_connection(stream);
+    }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let buf_reader = BufReader::new(&stream);
+    let request_line = buf_reader.lines().next().unwrap().unwrap();
+
+    let (status_line, filename) = if request_line == "GET / HTTP/1.1" {
+        ("HTTP/1.1 200 OK", "hello.html")
+    } else {
+        ("HTTP/1.1 404 NOT FOUND", "404.html")
+    };
+
+    let contents = fs::read_to_string(filename).unwrap();
+    let length = contents.len();
+    let content_type = " text/html; charset=utf-8";
+
+    let response =
+            format!("{status_line}\r\nContent-Length: {length}\r\nContent-Type:{content_type}\r\n\r\n{contents}");
+
+    stream.write_all(response.as_bytes()).unwrap();
 }
